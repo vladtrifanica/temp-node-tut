c573e5f0f11bcdc4ef70a1fe7958455fc0597e08
vlad.trifanica <vlad.trifanica@itperspectives.ro>
2021-05-06 15:37:53 +0200

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,31 @@
+const { readFile, writeFile } = require('fs');
+
+console.log('Start');
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const first = result;
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const second = result;
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result: ${first}, ${second}`,
+            { flag: 'a' },
+            (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Done with this task');
+            });
+    })
+});
+
+console.log('Starting with next task');
